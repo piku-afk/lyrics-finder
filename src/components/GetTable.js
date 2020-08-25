fee89c2d1406526075efa1d0014b9f8ec9a18e8d
client33 <51166024+client33@users.noreply.github.com>
2020-08-25 13:03:34 +0530

    Trending Page complete

---
+++ src/components/GetTable.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { TableRow, TableCell, Typography, Avatar, Grid, TableHead, TableBody, makeStyles, Table, Paper } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+
+
+const useStyles = makeStyles({
+  parentPaper: {
+    marginTop: 10
+  },
+  tableHead: {
+    border: 'none',
+    padding: '20px 16px'
+  },
+  h2: {
+    fontSize: 22
+  }
+});
+
+const link = {
+  color: 'inherit',
+  textDecoration: 'none'
+}
+
+
+
+function getTableRow(track, index, song) {
+
+  const imgSrc = song ? 
+  `https://api.napster.com/imageserver/v2/albums/${track.albumId}/images/170x170.jpg` : 
+  `https://api.napster.com/imageserver/v2/artists/${track.id}/images/150x100.jpg`;
+
+  return (
+    <TableRow key={track.id} >
+      <TableCell>
+        <Link style={link} to={song ? `track/${track.id}` : `artist/${track.id}`} >
+          <Grid container spacing={2} alignItems='center' >
+            <Grid item xs={1}>
+              {index + 1}
+            </Grid>
+            <Grid item xs={2}  >
+              <Avatar variant={song ? 'square' : 'circle'} src={imgSrc} alt={track.name} />
+            </Grid>
+            <Grid item xs={9}>
+              <Typography variant='body1' >
+                {track.name}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Link>
+      </TableCell>
+    </TableRow>
+  );
+}
+
+export default function GetTable({title, data, song}) {
+
+  const classes = useStyles();
+
+  return (
+
+      <Paper classes={{
+        elevation1: classes.parentPaper
+      }} elevation={1} square>
+        <Table size='small' >
+          <TableHead>
+            <TableRow>
+              <TableCell variant='head' classes={{
+                head: classes.tableHead
+              }}>
+                <Typography variant='h2' classes={{
+                  h2: classes.h2
+                }} >
+                  {title}
+                </Typography>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+    
+          <TableBody>
+            {data.map((item, index) => getTableRow(item, index, song))}
+          </TableBody>
+        </Table>
+      </Paper>
+  );
+};
